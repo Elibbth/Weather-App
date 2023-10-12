d3ba316f724ecad459813b2cd9dbf99b5d0f5336
Eli Leyder <eli.leyder@gmail.com>
2023-10-12 16:22:07 +0200

    test button enter

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -9,8 +9,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const cityName = document.getElementById('cityName');   //input
     let mypic = "";
 
-    searchButton.addEventListener('click', () => {
 
+    function loading() { //Cela déclare une fonction appelée "loading". Cette fonction sera exécutée ultérieurement lorsque l'événement "load" de la fenêtre (page web) se produira.
+        window.addEventListener("load", () => { // nous ajoutons un écouteur d'événements à l'objet "window" (la fenêtre du navigateur). L'événement que nous écoutons est "load", qui se produit lorsque la page web est entièrement chargée. Lorsque cet événement se déclenche, la fonction fléchée (lambda) suivante est exécutée.
+            let recupInput = cityName.value; //Cette ligne déclare une variable "recupInput" et y stocke la valeur de la variable "cityName". Il est important de noter que "cityName" n'est pas défini dans cette fonction, ce qui signifie qu'il doit être défini ailleurs dans le code. Cela suppose que "cityName" est un élément de formulaire (comme un champ de saisie de texte) et que cette ligne récupère sa valeur.
+            const cityInput = localStorage.getItem("city"); // Ici, nous déclarons une variable constante "cityInput" et y stockons la valeur associée à la clé "city" dans le stockage local du navigateur. Le stockage local est une fonctionnalité du navigateur qui permet de stocker des données côté client de manière persistante.
+            if (recupInput) { //Cette ligne commence une structure de contrôle conditionnelle (un "if") qui vérifie si la variable "recupInput" a une valeur. Si "recupInput" est évaluée comme vraie (c'est-à-dire si elle n'est pas nulle, indéfinie, vide, ou évaluée à "false"), alors le code à l'intérieur de cette condition sera exécuté.
+                card(cityInput); //Si la condition précédente est vraie, cette ligne appelle une fonction "card" avec "cityInput" en tant qu'argument. Cela suppose qu'il existe une fonction "card" ailleurs dans le code qui prend un argument (peut-être pour afficher des informations liées à la ville).
+            }
+        })
+    }
+    loading(); // Enfin, cette ligne appelle la fonction "loading" immédiatement après sa définition. Cela déclenche le processus d'ajout de l'écouteur d'événements pour l'événement "load" de la fenêtre, ce qui entraînera l'exécution du code à l'intérieur de la fonction "loading" lorsque la page est complètement chargée.
+
+    searchButton.addEventListener('click', () => {
+        let recupInput = cityName.value;
+        localStorage.setItem("city", recupInput);
         fetch(`https://api.unsplash.com/search/photos?page=1&per_page=1&query=${cityName.value}&client_id=${unsplashKey}`) // Remplacez 'your-json-file.json' par le 'chemin de votre fichier JSON' ou API
             .then(response => response.json()) //les réponses sont traitées comme un doc Json
             .then(data => {  // les reponses constituent une serie de données
@@ -86,4 +99,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
             });
     })
+    // cityName.addEventListener("keyup", (e) => {
+    //     if (e.key==="enter") {
+    //         e.preventDefault();
+
+        
+    
 });
